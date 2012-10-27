d031b2161477e96815d9e1f0b89c936ef404d940
Ken David <ken.s.david@gmail.com>
2012-10-27 09:42:45 -0400

    Added bouncing off the walls and rotating

---
+++ Nov01/View.h
@@ -0,0 +1,13 @@
+//
+//  View.h
+//  Nov01
+//
+//  Created by Kenneth David on 10/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface View : UIView
+
+@end
